f69471d76d58f759f2d9b4bea6f96dcfcb084f35
Fin <fantomma91@gmail.com>
2022-02-20 03:48:48 +0700

    fix  calc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const calcBtns = document.querySelectorAll('button[data-btn]');
+const calcBtns = document.querySelectorAll('[data-btn]');
 const resultInput = document.querySelector('.calc__result');
 
 let operator = '';
@@ -10,6 +10,28 @@ const OPERATORS = {
   '–': 'subt',
   '×': 'multi',
   '÷': 'div',
+}
+
+calcBtns.forEach(calcBtn => calcBtn.addEventListener('click', definitionAction));
+
+function definitionAction(e) {
+  const errorReset = (resultInput.value === 'Error' && e.currentTarget.dataset.btn !== 'reset')
+  if (errorReset) return;
+  changeFontSize()
+  switch (e.currentTarget.dataset.btn) {
+    case 'del':
+      return deleteChar(), changeFontSize();
+    case 'num':
+      return addChar(e), changeFontSize();
+    case 'reset':
+      return outputOnDisplay(), changeFontSize();
+    case 'equals':
+      return getResult(), changeFontSize();
+    case 'operator':
+      return addOperator(e), changeFontSize();
+    default:
+      break;
+  }
 }
 
 function Calc(operation, a, b) {
@@ -34,25 +56,20 @@ function changeFontSize() {
   }
 }
 
-function getResult(e) {
-  if (e.currentTarget.dataset.btn !== 'equals') return;
-
+function getResult() {
   if (num2 === '') return;
   if (operator in OPERATORS) {
     num1 = Calc(OPERATORS[operator], +num1, +num2);
-    if (resultInput.value === 'Error') return;
     outputOnDisplay(num1);
   }
 }
 
 function addOperator(e) {
-  if (e.currentTarget.dataset.btn !== 'operator') return;
   !operator && (operator = e.currentTarget.textContent);
 
   for (const key in OPERATORS) {
     if ((num2 || num2 === 0) && operator === key) {
       num1 = Calc(OPERATORS[operator], +num1, +num2);
-      if (resultInput.value === 'Error') return;
       operator = e.currentTarget.textContent;
       outputOnDisplay(num1, operator);
       return;
@@ -64,7 +81,6 @@ function addOperator(e) {
       return;
     }
   }
-  if (resultInput.value === 'Error') return;
   resultInput.value += operator;
 }
 
@@ -75,58 +91,26 @@ function outputOnDisplay(numOne, op, numTwo) {
   resultInput.value = `${num1.toLocaleString('en-US', {maximumFractionDigits: 20})}${operator}${num2.toLocaleString('en-US', {maximumFractionDigits: 20})}`;
 }
 
-function deleteChar(e) {
-  if (e.currentTarget.dataset.btn !== 'del') return;
-
-  if (resultInput.value === 'Error') return;
+function deleteChar() {
   const reduceNum = (num) => String(num).length === 1 ? '' : +String(num).slice(0, length - 1);
   const isNegativeZero = (String(num1).length === 2 && String(num1)[0] === '-');
   const isFraction = num1 % 1;
+  const isOperator = (!num2 && operator);
 
   if (isFraction) {
     num1 = reduceNum(num1);
     return outputOnDisplay(num1);
   }
-
-  if (isNegativeZero) {
-    return outputOnDisplay();
-  }
-
-  if (!num2 && operator || num1 < 0) {
-    if (operator) return outputOnDisplay(num1);
-    num1 = reduceNum(num1);
-    return outputOnDisplay(num1);
-  }
+  if (isNegativeZero) return outputOnDisplay();
+  if (isOperator) return outputOnDisplay(num1);
 
   !operator ? (num1 = reduceNum(num1) || 0) : (num2 = reduceNum(num2))
   outputOnDisplay(num1, operator, num2);
 }
 
 function addChar(e) {
-  if (e.currentTarget.dataset.btn !== 'num') return;
-
-  if (resultInput.value === 'Error') return;
   const increaseNum = (num) => (String(num).length < 6) ? (num += e.currentTarget.textContent) : num;
-
   !operator ? (num1 = +increaseNum(num1)) : (num2 = +increaseNum(num2));
-
   (num2 === 0) && (num2 = e.currentTarget.textContent);
-
   outputOnDisplay(num1, operator, num2);
 }
-
-calcBtns.forEach(calcBtn => {
-  calcBtn.addEventListener('click', function (e) {
-    if (e.currentTarget.dataset.btn !== 'reset') return;
-    outputOnDisplay();
-  });
-  calcBtn.addEventListener('click', function (e) {
-    (e.currentTarget.textContent in OPERATORS) ? addOperator(e): getResult(e);
-  });
-  calcBtn.addEventListener('click', deleteChar);
-
-  calcBtn.addEventListener('click', addChar);
-  calcBtn.addEventListener('click', changeFontSize);
-});
-
-// console.log(`NUM num1: ${num1}, num2: ${num2}, operator: ${operator}`);
