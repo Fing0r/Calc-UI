75cfa801b0fe9a660d9b7798f72ff130a318d576
Fin <fantomma91@gmail.com>
2022-02-19 17:06:42 +0700

    Add data-btn

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-const calcBtnsNum = document.querySelectorAll('.calc__btn--num');
-const calcBtns = document.querySelectorAll('.calc__btn');
-const calcBtnDel = document.querySelector('.calc__btn--del');
-const calcBtnReset = document.querySelector('.calc__btn--reset');
-const calcBtnOperators = document.querySelectorAll('.calc__btn--operator');
-const operationInput = document.querySelector('.calc__operation');
+const calcBtns = document.querySelectorAll('button[data-btn]');
 const resultInput = document.querySelector('.calc__result');
 
 let operator = '';
@@ -29,38 +24,42 @@ function Calc(operation, a, b) {
 
 function changeFontSize() {
   if (resultInput.value.length > 5 && resultInput.value.length < 9) {
-    resultInput.style.fontSize = '60px'
-  } else if (resultInput.value.length >= 9 && resultInput.value.length < 16) {
-    resultInput.style.fontSize = '40px'
-  } else if (resultInput.value.length >= 16) {
-    resultInput.style.fontSize = '20px'
+    resultInput.style.fontSize = '60px';
+  } else if (resultInput.value.length >= 9 && resultInput.value.length < 15) {
+    resultInput.style.fontSize = '40px';
+  } else if (resultInput.value.length >= 15) {
+    resultInput.style.fontSize = '20px';
   } else {
-    resultInput.style.fontSize = null
+    resultInput.style.fontSize = null;
   }
 }
 
-function getResult() {
+function getResult(e) {
+  if (e.currentTarget.dataset.btn !== 'equals') return;
+
+  if (num2 === '') return;
   if (operator in OPERATORS) {
-    num1 = Calc(OPERATORS[operator], +num1, +num2)
+    num1 = Calc(OPERATORS[operator], +num1, +num2);
     if (resultInput.value === 'Error') return;
-    outputOnDisplay(num1)
+    outputOnDisplay(num1);
   }
 }
 
-function addOperator(btn) {
-  !operator && (operator = btn.textContent);
+function addOperator(e) {
+  if (e.currentTarget.dataset.btn !== 'operator') return;
+  !operator && (operator = e.currentTarget.textContent);
 
   for (const key in OPERATORS) {
     if ((num2 || num2 === 0) && operator === key) {
-      num1 = Calc(OPERATORS[operator], +num1, +num2)
+      num1 = Calc(OPERATORS[operator], +num1, +num2);
       if (resultInput.value === 'Error') return;
-      operator = btn.textContent;
+      operator = e.currentTarget.textContent;
       outputOnDisplay(num1, operator);
       return;
     }
 
     if (resultInput.value.includes(key)) {
-      operator = btn.textContent;
+      operator = e.currentTarget.textContent;
       resultInput.value = resultInput.value.slice(0, length - 1) + operator;
       return;
     }
@@ -73,55 +72,61 @@ function outputOnDisplay(numOne, op, numTwo) {
   !op && (operator = '');
   !numTwo && (num2 = '');
   !numOne && (num1 = 0);
-  resultInput.value = `${num1.toLocaleString('en-US', {maximumFractionDigits: 20}) }${operator}${num2.toLocaleString('en-US', {maximumFractionDigits: 20}) }`
+  resultInput.value = `${num1.toLocaleString('en-US', {maximumFractionDigits: 20})}${operator}${num2.toLocaleString('en-US', {maximumFractionDigits: 20})}`;
 }
 
-calcBtnDel.addEventListener('click', function () {
+function deleteChar(e) {
+  if (e.currentTarget.dataset.btn !== 'del') return;
+
   if (resultInput.value === 'Error') return;
   const reduceNum = (num) => String(num).length === 1 ? '' : +String(num).slice(0, length - 1);
   const isNegativeZero = (String(num1).length === 2 && String(num1)[0] === '-');
   const isFraction = num1 % 1;
 
   if (isFraction) {
-    num1 = reduceNum(num1)
+    num1 = reduceNum(num1);
     return outputOnDisplay(num1);
   }
 
   if (isNegativeZero) {
-    return outputOnDisplay()
+    return outputOnDisplay();
   }
 
   if (!num2 && operator || num1 < 0) {
-    if (operator) return outputOnDisplay(num1)
-    num1 = reduceNum(num1)
-    return outputOnDisplay(num1)
+    if (operator) return outputOnDisplay(num1);
+    num1 = reduceNum(num1);
+    return outputOnDisplay(num1);
   }
 
   !operator ? (num1 = reduceNum(num1) || 0) : (num2 = reduceNum(num2))
-  outputOnDisplay(num1, operator, num2)
-});
+  outputOnDisplay(num1, operator, num2);
+}
 
+function addChar(e) {
+  if (e.currentTarget.dataset.btn !== 'num') return;
 
-calcBtnsNum.forEach(function (calcBtnNum) {
-  calcBtnNum.addEventListener('click', function () {
-    if (resultInput.value === 'Error') return;
-    const increaseNum = (num) => (String(num).length < 6) ? (num += calcBtnNum.textContent) : num;
+  if (resultInput.value === 'Error') return;
+  const increaseNum = (num) => (String(num).length < 6) ? (num += e.currentTarget.textContent) : num;
 
-    !operator ? (num1 = +increaseNum(num1)) : (num2 = +increaseNum(num2));
-    outputOnDisplay(num1, operator, num2)
-  });
-});
+  !operator ? (num1 = +increaseNum(num1)) : (num2 = +increaseNum(num2));
 
-calcBtnOperators.forEach(function (calcBtnOperator) {
-  calcBtnOperator.addEventListener('click', function () {
-    (calcBtnOperator.textContent in OPERATORS) ? addOperator(calcBtnOperator): getResult();
-  });
-});
+  (num2 === 0) && (num2 = e.currentTarget.textContent);
 
-calcBtnReset.addEventListener('click', function () {
-  outputOnDisplay();
-})
+  outputOnDisplay(num1, operator, num2);
+}
 
 calcBtns.forEach(calcBtn => {
-  calcBtn.addEventListener('click', changeFontSize)
+  calcBtn.addEventListener('click', function (e) {
+    if (e.currentTarget.dataset.btn !== 'reset') return;
+    outputOnDisplay();
+  });
+  calcBtn.addEventListener('click', function (e) {
+    (e.currentTarget.textContent in OPERATORS) ? addOperator(e): getResult(e);
+  });
+  calcBtn.addEventListener('click', deleteChar);
+
+  calcBtn.addEventListener('click', addChar);
+  calcBtn.addEventListener('click', changeFontSize);
 });
+
+// console.log(`NUM num1: ${num1}, num2: ${num2}, operator: ${operator}`);
